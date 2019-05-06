2c8e4d09d0cd4e7b2ef36a45085615646602a284
OctopiWithHats <sdgranoff@gmail.com>
2019-05-06 11:02:48 -0400

    quick working validations for alignments and powers weapons

--- Brawlmighty/app/models/character.rb
+++ Brawlmighty/app/models/character.rb
@@ -4,4 +4,27 @@ class Character < ApplicationRecord
   belongs_to :weapon
   belongs_to :terrain
   belongs_to :user
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
+  validates :terrain, presence: true
+
+  validates :user, presence: true
+
+  validate :power_alignment_match
+  validate :weapon_alignment_match
+
+
+  def power_alignment_match
+    unless self.power.alignment == self.alignment
+      errors.add(:power, "Power must match with alignment")
+    end
+  end
+
+  def weapon_alignment_match
+    unless self.weapon.alignment == self.alignment
+      errors.add(:weapon, "Weapon must match with alignment")
+    end
+  end
 end
